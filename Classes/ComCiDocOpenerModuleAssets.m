957c60d52e9f1b70d87e2fd16c1d8bd4bdfdd5f3
Aaron Saunders <aaron@clearlyinnovative.com>
2011-04-12 23:37:45 -0400

    initial checkin for working module

---
+++ Classes/ComCiDocOpenerModuleAssets.m
@@ -0,0 +1,15 @@
+/**
+ * This is a generated file. Do not edit or your changes will be lost
+ */
+#import "ComCiDocOpenerModuleAssets.h"
+
+extern NSData * dataWithHexString (NSString * hexString);
+
+@implementation ComCiDocOpenerModuleAssets
+
+- (NSData*) moduleAsset
+{
+	return nil;
+}
+
+@end
