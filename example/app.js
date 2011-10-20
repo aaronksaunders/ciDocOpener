a7654117095049126b35f62c83286701cfa3887d
Aaron Saunders <aaron@clearlyinnovative.com>
2011-10-20 01:47:10 -0400

    0.2 marketplace release

--- example/app.js
+++ example/app.js
@@ -1,41 +1,72 @@
 // This is a test harness for your module
-// You should do something interesting in this harness 
-// to test out the module and to provide instructions 
+// You should do something interesting in this harness
+// to test out the module and to provide instructions
 // to users on how to use it by example.
 
-
-// open a single window
-var window = Ti.UI.createWindow({
-	backgroundColor:'white'
+var bw = Ti.UI.createWindow({
+    backgroundColor : 'white'
 });
-var label = Ti.UI.createLabel();
-window.add(label);
-window.open();
+
+var w = Ti.UI.createWindow({
+    backgroundColor : 'white',
+    title : "ciDocOpener Test App"
+});
+
+var ng = Ti.UI.iPhone.createNavigationGroup({
+    window : w
+});
 
 // TODO: write your module tests here
 var ciDocOpener = require("com.ci.docOpener");
 Ti.API.info("module is => " + ciDocOpener);
 
-Ti.API.info(Ti.Filesystem.getFile(Ti.Filesystem.resourcesDirectory + "/dcps012.pdf").nativePath);
-
-var f = Titanium.Filesystem.getFile(Titanium.Filesystem.resourcesDirectory, 'dcps012.pdf');
-var tempFile = Titanium.Filesystem.getFile(Titanium.Filesystem.applicationDataDirectory,'dcps012.pdf');
-tempFile.write(f.read());
-
-Ti.API.info("temp file "+tempFile.nativePath);
-
-button = Ti.UI.createButton({
-    top: 10,
-    left: 10,
-    height: 30,
-    width: 100,
-    title: "button"
+var f, tempFile, button_pdf, button_ppt, image_view;
+image_view = Ti.UI.createView({
+    top : 0,
+    left : 0,
+    height : 77,
+    width : 320,
+    backgroundImage : 'new_logo.jpg'
 });
-window.add(button);
+w.add(image_view);
 
-button.addEventListener('click',
-function(e) {
+// buttons
+button_pdf = Ti.UI.createButton({
+    top : 100,
+    left : 90,
+    height : 30,
+    width : 150,
+    title : "View PDF"
+});
+w.add(button_pdf);
+button_ppt = Ti.UI.createButton({
+    top : 150,
+    left : 90,
+    height : 30,
+    width : 150,
+    title : "View PPT"
+});
+w.add(button_ppt);
+
+button_pdf.addEventListener('click', function(e) {
+    f = Titanium.Filesystem.getFile('Module_Developers_Guide_iOS.pdf');
+    tempFile = Titanium.Filesystem.getFile(Titanium.Filesystem.applicationDataDirectory, 'Module_Developers_Guide_iOS.pdf');
+    tempFile.write(f.read());
+
     ciDocOpener.useDocumentOpener({
-        path: tempFile.nativePath
+        path : tempFile.nativePath
     });
 });
+
+button_ppt.addEventListener('click', function(e) {
+    f = Titanium.Filesystem.getFile('280.ppt');
+    tempFile = Titanium.Filesystem.getFile(Titanium.Filesystem.applicationDataDirectory, '280.ppt');
+    tempFile.write(f.read());
+
+    ciDocOpener.useDocumentOpener({
+        path : tempFile.nativePath
+    });
+});
+
+bw.add(ng);
+bw.open();
